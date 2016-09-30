d54d97cd2a3672b24d3902d5135fca0d7839dd0b
Drew Keller <dkeller@pillartechnology.com>
2016-09-30 14:58:40 -0400

    Suppress default constructor

--- src/main/java/com/eightycolumns/berlin-clock/BerlinClock.java
+++ src/main/java/com/eightycolumns/berlin-clock/BerlinClock.java
@@ -1,6 +1,10 @@
 package com.eightycolumns.berlin_clock;
 
 class BerlinClock {
+  private BerlinClock() {
+    // Suppress default constructor
+  }
+
   public static String secondsLamp(int second) {
     return (second % 2 == 0) ? "1" : "0";
   }
