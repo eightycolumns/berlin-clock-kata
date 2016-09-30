03f3f3e10c986e19f0e5368486a9f0f8ea1dcf48
Drew Keller <dkeller@pillartechnology.com>
2016-09-30 12:00:38 -0400

    Introduce n_lights_off

--- src/main/java/com/eightycolumns/berlin-clock/BerlinClock.java
+++ src/main/java/com/eightycolumns/berlin-clock/BerlinClock.java
@@ -8,29 +8,33 @@ class BerlinClock {
   public static String fiveHourRow(int hour) {
     int n_lights = 4;
     int n_lights_on = hour / 5;
+    int n_lights_off = n_lights - n_lights_on;
 
-    return repeat('1', n_lights_on) + repeat('0', n_lights - n_lights_on);
+    return repeat('1', n_lights_on) + repeat('0', n_lights_off);
   }
 
   public static String oneHourRow(int hour) {
     int n_lights = 4;
     int n_lights_on = hour % 5;
+    int n_lights_off = n_lights - n_lights_on;
 
-    return repeat('1', n_lights_on) + repeat('0', n_lights - n_lights_on);
+    return repeat('1', n_lights_on) + repeat('0', n_lights_off);
   }
 
   public static String fiveMinuteRow(int minute) {
     int n_lights = 11;
     int n_lights_on = minute / 5;
+    int n_lights_off = n_lights - n_lights_on;
 
-    return repeat('1', n_lights_on) + repeat('0', n_lights - n_lights_on);
+    return repeat('1', n_lights_on) + repeat('0', n_lights_off);
   }
 
   public static String oneMinuteRow(int minute) {
     int n_lights = 4;
     int n_lights_on = minute % 5;
+    int n_lights_off = n_lights - n_lights_on;
 
-    return repeat('1', n_lights_on) + repeat('0', n_lights - n_lights_on);
+    return repeat('1', n_lights_on) + repeat('0', n_lights_off);
   }
 
   private static String repeat(char c, int n) {
