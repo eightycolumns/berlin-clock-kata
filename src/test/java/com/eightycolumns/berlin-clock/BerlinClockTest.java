d1e642adb28b754e0a9f0446a8a349196c67c38b
Drew Keller <dkeller@pillartechnology.com>
2016-10-10 12:04:39 -0400

    Extract FiveMinuteRowTest

--- src/test/java/com/eightycolumns/berlin-clock/BerlinClockTest.java
+++ src/test/java/com/eightycolumns/berlin-clock/BerlinClockTest.java
@@ -11,52 +11,6 @@ public class BerlinClockTest {
   @Rule
   public ExpectedException thrown = ExpectedException.none();
 
-  // Five-minute row
-
-  @Test
-  public void fiveMinuteRowExpectsNonnegativeMinute() {
-    thrown.expect(IllegalArgumentException.class);
-    thrown.expectMessage("Invalid minute");
-    BerlinClock.fiveMinuteRow(-1);
-  }
-
-  @Test
-  public void fiveMinuteRowIs00000000000AtMinute0() {
-    assertEquals("00000000000", BerlinClock.fiveMinuteRow(0));
-  }
-
-  @Test
-  public void fiveMinuteRowIs00000000000AtMinute4() {
-    assertEquals("00000000000", BerlinClock.fiveMinuteRow(4));
-  }
-
-  @Test
-  public void fiveMinuteRowIs10000000000AtMinute5() {
-    assertEquals("10000000000", BerlinClock.fiveMinuteRow(5));
-  }
-
-  @Test
-  public void fiveMinuteRowIs10000000000AtMinute9() {
-    assertEquals("10000000000", BerlinClock.fiveMinuteRow(9));
-  }
-
-  @Test
-  public void fiveMinuteRowIs11000000000AtMinute10() {
-    assertEquals("11000000000", BerlinClock.fiveMinuteRow(10));
-  }
-
-  @Test
-  public void fiveMinuteRowIs11111111111AtMinute59() {
-    assertEquals("11111111111", BerlinClock.fiveMinuteRow(59));
-  }
-
-  @Test
-  public void fiveMinuteRowExpectsMinuteLessThan60() {
-    thrown.expect(IllegalArgumentException.class);
-    thrown.expectMessage("Invalid minute");
-    BerlinClock.fiveMinuteRow(60);
-  }
-
   // One-minute row
 
   @Test
