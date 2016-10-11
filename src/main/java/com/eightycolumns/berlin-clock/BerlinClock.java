789f0bb20fa3057f39312531abe5dc43b27c02db
Drew Keller <drew@eightycolumns.com>
2016-10-10 20:36:49 -0400

    Remove vestigial methods

--- src/main/java/com/eightycolumns/berlin-clock/BerlinClock.java
+++ src/main/java/com/eightycolumns/berlin-clock/BerlinClock.java
@@ -14,28 +14,4 @@ class BerlinClock {
       OneMinuteRow.atMinute(minute)
     );
   }
-
-  private static boolean isEven(int d) {
-    return d % 2 == 0;
-  }
-
-  private static String row(int n_lights, int n_lights_on) {
-    assert n_lights >= n_lights_on;
-
-    int n_lights_off = n_lights - n_lights_on;
-
-    return repeat('1', n_lights_on) + repeat('0', n_lights_off);
-  }
-
-  private static String repeat(char c, int n) {
-    assert n >= 0;
-
-    StringBuilder sb = new StringBuilder();
-
-    for (int i = 0; i < n; i += 1) {
-      sb.append(c);
-    }
-
-    return sb.toString();
-  }
 }
