5de4b490e9f7223fe1c9d861e75b4375134ba9f5
Drew Keller <drew@eightycolumns.com>
2016-10-11 06:47:37 -0400

    Make BerlinClock instantiable

--- src/main/java/com/eightycolumns/berlin-clock/BerlinClock.java
+++ src/main/java/com/eightycolumns/berlin-clock/BerlinClock.java
@@ -1,11 +1,7 @@
 package com.eightycolumns.berlin_clock;
 
 class BerlinClock {
-  private BerlinClock() {
-    // Suppress default constructor
-  }
-
-  public static String compositeRow(int hour, int minute, int second) {
+  public String compositeRow(int hour, int minute, int second) {
     return (
       SecondsLamp.atSecond(second) +
       FiveHourRow.atHour(hour) +
--- src/test/java/com/eightycolumns/berlin-clock/CompositeRowTest.java
+++ src/test/java/com/eightycolumns/berlin-clock/CompositeRowTest.java
@@ -7,47 +7,57 @@ import org.junit.Test;
 public class CompositeRowTest {
   @Test(expected = InvalidHourException.class)
   public void compositeRowExpectsNonnegativeHour() {
-    BerlinClock.compositeRow(-1, 0, 0);
+    BerlinClock berlinClock = new BerlinClock();
+    berlinClock.compositeRow(-1, 0, 0);
   }
 
   @Test(expected = InvalidMinuteException.class)
   public void compositeRowExpectsNonnegativeMinute() {
-    BerlinClock.compositeRow(0, -1, 0);
+    BerlinClock berlinClock = new BerlinClock();
+    berlinClock.compositeRow(0, -1, 0);
   }
 
   @Test(expected = InvalidSecondException.class)
   public void compositeRowExpectsNonnegativeSecond() {
-    BerlinClock.compositeRow(0, 0, -1);
+    BerlinClock berlinClock = new BerlinClock();
+    berlinClock.compositeRow(0, 0, -1);
   }
 
   @Test
   public void compositeRowIs100000000000000000000000AtHour0Minute0Second0() {
+    BerlinClock berlinClock = new BerlinClock();
+
     assertEquals(
       "100000000000000000000000",
-      BerlinClock.compositeRow(0, 0, 0)
+      berlinClock.compositeRow(0, 0, 0)
     );
   }
 
   @Test
   public void compositeRowIs011111110111111111111111AtHour23Minute59Second59() {
+    BerlinClock berlinClock = new BerlinClock();
+
     assertEquals(
       "011111110111111111111111",
-      BerlinClock.compositeRow(23, 59, 59)
+      berlinClock.compositeRow(23, 59, 59)
     );
   }
 
   @Test(expected = InvalidHourException.class)
   public void compositeRowExpectsHourLessThan24() {
-    BerlinClock.compositeRow(24, 0, 0);
+    BerlinClock berlinClock = new BerlinClock();
+    berlinClock.compositeRow(24, 0, 0);
   }
 
   @Test(expected = InvalidMinuteException.class)
   public void compositeRowExpectsMinuteLessThan60() {
-    BerlinClock.compositeRow(0, 60, 0);
+    BerlinClock berlinClock = new BerlinClock();
+    berlinClock.compositeRow(0, 60, 0);
   }
 
   @Test(expected = InvalidSecondException.class)
   public void compositeRowExpectsSecondLessThan60() {
-    BerlinClock.compositeRow(0, 0, 60);
+    BerlinClock berlinClock = new BerlinClock();
+    berlinClock.compositeRow(0, 0, 60);
   }
 }
